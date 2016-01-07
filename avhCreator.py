233e75aad79d0992425bfc654ccbfee2e8ea7e1b
pymalch <malekiciw@gmail.com>
2016-01-08 02:20:46 +0330

    fix the user owner issue and set a little more settings

--- avhCreator.py
+++ avhCreator.py
@@ -10,33 +10,40 @@ def msg(stat):
 def newline():
 	print ""
 
-def new_hosts(domain):
-	msg(" What would be the public directory name? \n - Press enter to keep default name (\"public\") ")
-	public_dir = raw_input()
+def getUser():
+	if os.environ.has_key('SUDO_USER'):
+		  return os.environ['SUDO_USER']
+	else:
+		  return os.environ['USER']
+
+def new_hosts(domain,public_dir,www,user):
+	msg(" What would be the public directory name? \n - Press enter to keep default name (\""+public_dir+"\") ")
+	public_dir_temp = raw_input()
 
 	# Chceck and set name of the public directory.
-	if public_dir == "":
-	    public_dir = "public"
+	if public_dir_temp !="":
+	    public_dir = public_dir_temp
 
 	newline()
 
 	msg(" Creating the Directory Structure ")
-	os.system("sudo mkdir -p /var/www/"+domain+"/"+public_dir)
+	os.system("sudo mkdir -p "+www+domain+"/"+public_dir)
 
 	newline()
 
 	msg(" Granting Proper Permissions ")
-	os.system("sudo chown -R $USER:$USER /var/www/"+domain+"/"+public_dir)
+	msg("sudo chown -R "+user+" "+www+domain)
+	os.system("sudo chown -R "+user+" "+www+domain)
 
 	newline()
 
 	msg(" Making Sure Read Access is Permitted ")
-	os.system("sudo chmod -R 755 /var/www")
+	os.system("sudo chmod -R 755 "+www)
 
 	newline()
 
 	msg(" Adding A Demo Page ")
-	file_object = open("/var/www/"+domain+"/"+public_dir+"/index.html", "w")
+	file_object = open(www+domain+"/"+public_dir+"/index.html", "w")
 	file_object.write("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Virtual Hosts Created Successfully!</title><style>html{background-color: #508bc9;color: #fff;font-family: sans-serif, arial;}.container{width: 80%;margin: auto auto;}.inl{text-align: center;}.inl img{border-radius: 10px;}a{color: #f2d8ab;}</style></head><body><div class='container'><h1>Virtual Hosts Created Successfully!</h1><p><b>Apache Virtual Hosts Generator</b> has successfully created a virtual host in your server.<br>We can code it better! Join at <a href='https://github.com/rakibtg/Apache-Virtual-Hosts-Creator' target='_blank'>GitHub</a><br>Created by <a href='https://www.twitter.com/rakibtg' target='_blank'>Hasan</a></p><div class='divider'><div class='inl'><h1>Let's celebrate!</h1><img src='http://i.imgur.com/vCbBhwy.gif' alt='Scene from Spider Man Movie (C) Spider Man Movie ..'></div></div></div></body></html>")
 	file_object.close()
 
@@ -44,7 +51,7 @@ def new_hosts(domain):
 
 	msg(" Creating Virtual Host File ")
 	host_file = open("/tmp/"+domain+".conf", "w")
-	host_file.write("<VirtualHost *:80>\nServerAdmin localserver@localhost\nServerName "+domain+"\nServerAlias www."+domain+"\nDocumentRoot /var/www/"+domain+"/"+public_dir+"\nErrorLog ${APACHE_LOG_DIR}/error.log\nCustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>")
+	host_file.write("<VirtualHost *:80>\nServerAdmin localserver@localhost\nServerName "+domain+" \nServerAlias www."+domain+"\nDocumentRoot "+www+domain+"/"+public_dir+"\n<Directory "+www+domain+"/"+public_dir+">\nDirectoryIndex index.php\nOptions Indexes FollowSymLinks\nAllowOverride All\nRequire all granted\n</Directory>\nErrorLog ${APACHE_LOG_DIR}/error.log\nCustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>")
 	host_file.close()
 	os.system("sudo mv \"/tmp/"+domain+".conf\" \"/etc/apache2/sites-available/\"")
 
@@ -63,14 +70,18 @@ def new_hosts(domain):
 	newline()
 
 	msg(" Setting Up Local Host File ")
-	if host_flag == 0:
-		os.system("sudo sed -i -e '1i127.0.1.1   "+domain+"\' \"/etc/hosts\"")
-	else:
-		print " Skipped! "
+
+	os.system("sudo sed -i -e '1i127.0.1.1   "+domain+"\' \"/etc/hosts\"")
+
 
 	print "\nSuccess! Please visit http://"+domain+"/ from any web browser\n\n"
 
 host_flag = 0
+user = getUser()
+
+#config
+www="/home/%s/www/" % user
+public_dir = "public_html"
 
 newline()
 
@@ -78,10 +89,19 @@ print "\n Welcome to Apache Virtual Hosts Creator\n - This script will setup a A
 
 newline()
 
-msg(" What would be the domain name? ")
-domain = raw_input()
+domain=""
+while domain=="":
+	msg(" What would be the domain name? (you must set a name!) ")
+	domain = raw_input()
 
-if os.path.exists("/var/www/"+domain):
+msg("What would be the path of www root apache? \n - Press enter to keep default path (\""+www +"\")")
+www_temp=raw_input()
+
+if www_temp!="":
+	www=www_temp
+
+
+if os.path.exists(www+domain):
 	msg(" IMPORTANT: It seems that you have already configured a virtual hosts with the same domain name \n If you continue then all your data of http://"+domain+"/ will be overwritten and can not be undo \n Continue? (yes/no) ")
 	flag = raw_input()
 	host_flag = 1
@@ -93,6 +113,6 @@ if os.path.exists("/var/www/"+domain):
 	if flag == "yes":
 		newline()
 		msg(" Existing host will be overwritten ... ")
-		new_hosts(domain)
+		new_hosts(domain,public_dir,www,user)
 else:
-	new_hosts(domain)
+	new_hosts(domain,public_dir,www,user)
